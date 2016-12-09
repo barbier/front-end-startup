eda7166386b6a50f39bf87e5d4dcccf01ed5b169
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-12-09 18:48:48 -0200

    identation fixed dist folder add

--- broccoli/Brocfile.js
+++ broccoli/Brocfile.js
@@ -5,27 +5,27 @@ const funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
 
 const lib = babel('src/js', {
-	moduleIds: true,
-	modules: 'amd'
+  moduleIds: true,
+  modules: 'amd'
 });
 
 
 const appCss = new BroccoliSass(['src/sass'], 'app.scss', 'app.css');
 
 const indexHtml = funnel('src', {
-	files: ['index.html']
+  files: ['index.html']
 });
 
 const js_bundler = new WebpackWriter(lib, {
-	entry: './app',
-	output: {filename: 'app.bundle.js'},
-	devtool: 'source-map',
-	module: {
-		preLoaders: [{
-	    test: /\.js$/,
-		  loader: "source-map-loader"
-		}]
-	}
+  entry: './app',
+  output: {filename: 'app.bundle.js'},
+  devtool: 'source-map',
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: "source-map-loader"
+    }]
+  }
 });
 
 module.exports = mergeTrees([js_bundler, appCss, indexHtml]);
--- broccoli/src/js/data.js
+++ broccoli/src/js/data.js
@@ -1,3 +1,3 @@
 export default {
-	build: 'Broccoli-Webpack-Babel-Sass'
+  build: 'Broccoli-Webpack-Babel-Sass'
 }
